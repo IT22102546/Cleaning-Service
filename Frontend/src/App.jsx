20cb792999988cd09896f56e35888d441b2e8ff5
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-07-05 11:30:29 +0530

    Header created for Testing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 export default function App() {
   return (
    <BrowserRouter>
+       <Header/>
     <Routes>
+   
     <Route path="/" element={<Home/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
     </Routes>
    </BrowserRouter>
   )
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,109 @@
+import { Avatar, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, Navbar } from "flowbite-react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { HiUser, HiViewList } from 'react-icons/hi';
+import { useDispatch, useSelector } from "react-redux";
+import { signOut } from "../redux/user/userSlice";
+
+
+export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+ 
+
+  const handleSignOut = async () => {
+    try {
+      await fetch("/api/user/signout");
+      dispatch(signOut());
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Navbar className="border-b-2 relative z-50 bg-green-500">
+      <div className="container mx-auto flex flex-wrap items-center justify-between py-4">
+   
+        <div className="flex items-center">
+          <NavLink to="/" className="self-center whitespace-nowrap text-3xl font-semibold font-tangerine ml-0 md:ml-16">
+            Logo
+          </NavLink>
+        </div>
+        
+    
+        <div className="flex space-x-8">
+          <NavLink 
+            to="/" 
+            className={({ isActive }) => 
+              isActive ? "text-black" : "text-white"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink 
+            to="/category" 
+            className={({ isActive }) => 
+              isActive ? "text-black" : "text-white"
+            }
+          >
+            Categories
+          </NavLink>
+         
+          <NavLink 
+            to="/service-page" 
+            className={({ isActive }) => 
+              isActive ? "text-black" : "text-white"
+            }
+          >
+            Services
+          </NavLink>
+          
+          <NavLink 
+            to="/blogs" 
+            className={({ isActive }) => 
+              isActive ?"text-black" : "text-white"
+            }
+          >
+            Blogs
+          </NavLink>
+        </div>
+        
+        <div className="flex space-x-8 items-center">
+
+       
+          
+        {currentUser ? (
+                    <Dropdown arrowIcon={false} inline label={
+                      <Avatar alt="user" img={currentUser.profilePicture} rounded className="h-10 w-10" />
+                    }>
+                        <DropdownHeader>
+                            <span className="block text-sm">{currentUser.username}</span>
+                            <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+                        </DropdownHeader>
+                        <Link to={'/dashboard?tab=profile'}>
+                            <DropdownItem>Profile</DropdownItem>
+                        </Link>
+                        <DropdownDivider/>
+                        <DropdownItem onClick={handleSignOut}>Sign Out</DropdownItem>
+                    </Dropdown>
+                ) : (
+                    <Link to='/sign-in' className="text-white flex items-center mt-4 md:mt-0">
+                        <HiUser className="mr-1" style={{ fontSize: '24px' }} />
+                        Login/SignUp
+                    </Link>
+                )}
+
+                {currentUser && (
+                    <Link to="/wishlist">
+                    <div className="flex relative">
+                    <HiViewList className="mr-1" style={{ fontSize: '24px' }} />
+                        
+                    </div>
+                    </Link>
+                )}
+          
+        </div>
+      </div>
+    </Navbar>
+  );
+}
