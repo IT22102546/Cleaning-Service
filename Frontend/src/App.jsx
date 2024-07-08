0fcdffb64703ee18817805de079fb44e640d9884
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-07-08 19:46:48 +0530

    feat: added private route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,18 +3,24 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
-   <BrowserRouter>
-      <Header/>
-    <Routes>
+      <BrowserRouter>
+        <Header/>
+      <Routes>
+
       <Route path="/" element={<Home/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
       <Route path="/sign-in" element={<SignIn/>}/>
-    </Routes>
+
+        <Route element={<PrivateRoute/>}/>
+            <Route path="/dashboard" element={<DashBoard/>}/> 
+        <Route/>     
+      </Routes>
       <Footer/>
-   </BrowserRouter>
+    </BrowserRouter>
   )
 }
