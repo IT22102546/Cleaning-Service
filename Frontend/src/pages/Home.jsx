8e5662b9381879c9af13ec494309c7da6c7d7fba
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-07-08 09:53:10 +0530

    feat:hero section enhancement

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -10,8 +10,9 @@ export default function Home() {
               Enjoy the best cleaning service for your home or workplace
             </h1>
             <p className="text-gray-600 mb-6">
-              methnt description ekk oneee ...
+            Experience unmatched quality and reliability with our top-rated cleaning services. Our dedicated team ensures your space is pristine, healthy, and inviting. Trust us to make your home or office shine!
             </p>
+            
             <div className="flex justify-center lg:justify-start mb-4">
               <button className="bg-orange-500 text-white px-6 py-2 rounded-md hover:bg-orange-600 transition">
                 Book Now
@@ -21,7 +22,7 @@ export default function Home() {
               <div className="flex items-center">
                 <span className="material-icons text-orange-500">call</span>
                 <a href="tel:+01234567890" className="text-gray-800 ml-2">
-                  +01234567890
+                  +9411 456 7890
                 </a>
               </div>
             </div>
@@ -41,9 +42,9 @@ export default function Home() {
           </div>
           <div className="lg:w-1/2 mt-8 lg:mt-0">
             <div className="relative">
-          
+              <div className="mt-[-50px]">
                 <img src="/images/cleaner.png" alt="Cleaner" className="mx-auto lg:mx-0 self-center justify-center" />
-              
+              </div>
               <div className="absolute top-0 right-0 mt-4 mr-4">
                 <div className="bg-yellow-200 p-2 rounded-full shadow-md">
                   <img src="/images/facebook.png" alt="Facebook" className="w-4 h-4" />
