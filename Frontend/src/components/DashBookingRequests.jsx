580ce71dbfae951c2b6c4e1291f91f85807bff89
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-07-31 21:57:48 +0530

    feat:finished booking request status update

--- Frontend/src/components/DashBookingRequests.jsx
+++ Frontend/src/components/DashBookingRequests.jsx
@@ -3,26 +3,33 @@ import { useEffect, useState } from "react";
 import { HiGift, HiOutlineExclamationCircle } from "react-icons/hi";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { data } from "autoprefixer";
 
 export default function DashBookingRequests() {
   const { currentUser } = useSelector((state) => state.user);
   const [requests, setRequests] = useState([]);
   const [showModel, setShowModel] = useState(false);
-  const [bookIdToDelete, setBookIdToDelete] = useState('');
+  const [bookIdToDelete, setBookIdToDelete] = useState("");
   const [totalRequests, setTotalRequests] = useState(0);
-  const [lastMonthRequests, setlastMonthRequests] = useState(0);
-  const [searchTerm,  setSearchTerm] = useState('');
+  const [totalAcceptRequests, setTotalAcceptRequests] = useState(0);
+  const [totalFinishedRequests, setTotalFinishedRequests] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchRequests = async () => {
       try {
-        const res = await fetch(`/api/book/get-bookings?searchTerm=${searchTerm}`);
+        const res = await fetch(
+          `/api/book/get-bookings?searchTerm=${searchTerm}`
+        );
         const data = await res.json();
         if (res.ok) {
           setRequests(data.requests);
           setTotalRequests(data.totalRequests);
-          setlastMonthRequests(data.lastMonthRequests);
+          setTotalAcceptRequests(
+            data.requests.filter((req) => req.accepted).length
+          );
+          setTotalFinishedRequests(
+            data.requests.filter((req) => req.finished).length
+          );
         }
       } catch (error) {
         console.log(error.message);
@@ -30,85 +37,112 @@ export default function DashBookingRequests() {
     };
 
     fetchRequests();
-  }, [data]);
+  }, [searchTerm]);
 
   const handleDeleteBooking = async () => {
     setShowModel(false);
-    console.log(bookIdToDelete);
     try {
       const res = await fetch(`/api/book/delete-book/${bookIdToDelete}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       const data = await res.json();
-      if (!res.ok) {
-        console.log(data.message);
-      } else {
+      if (res.ok) {
         setRequests((prev) =>
-          prev.filter((product) => product._id !== bookIdToDelete)
+          prev.filter((request) => request._id !== bookIdToDelete)
         );
+        setTotalRequests((prev) => prev - 1);
+      } else {
+        console.log(data.message);
       }
     } catch (error) {
       console.log(error.message);
     }
   };
-  
 
-  
+  const handleAcceptChange = (id) => {
+    setRequests((prevRequests) =>
+      prevRequests.map((request) =>
+        request._id === id
+          ? { ...request, accepted: !request.accepted }
+          : request
+      )
+    );
+    setTotalAcceptRequests(
+      (prev) =>
+        prev +
+        (requests.find((request) => request._id === id).accepted ? -1 : 1)
+    );
+  };
 
-const formatDate = (dateString) => {
+  const handleFinishChange = (id) => {
+    setRequests((prevRequests) =>
+      prevRequests.map((request) =>
+        request._id === id
+          ? { ...request, finished: !request.finished }
+          : request
+      )
+    );
+    setTotalFinishedRequests(
+      (prev) =>
+        prev +
+        (requests.find((request) => request._id === id).finished ? -1 : 1)
+    );
+  };
+
+  const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const options = { year: "numeric", month: "long", day: "numeric" };
     return date.toLocaleDateString(undefined, options);
   };
 
   return (
-    <div className='mt-24 table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+    <div className="mt-24 table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       <section className="hero-section"></section>
 
       <div className="flex flex-row justify-center">
-
-      <div className='flex-wrap flex gap-4 justify-center p-3'>
-        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
-          <div className='flex justify-between'>
-            <div className=''>
-              <h3 className='text-gray-500 text-md uppercase'>Total Requests</h3>
-              <p className='text-2xl'>{totalRequests}</p>
+        <div className="flex-wrap flex gap-4 justify-center p-3">
+          <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
+            <div className="flex justify-between">
+              <div className="">
+                <h3 className="text-gray-500 text-md uppercase">
+                  Total Requests
+                </h3>
+                <p className="text-2xl">{totalRequests}</p>
+              </div>
+              <HiGift className="bg-orange-500 text-white rounded-full text-5xl p-3 shadow-lg" />
             </div>
-            <HiGift className='bg-red-600 text-white rounded-full text-5xl p-3 shadow-lg' />
           </div>
         </div>
-      </div>
 
-      <div className='flex-wrap flex gap-4 justify-center p-3'>
-        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
-          <div className='flex justify-between'>
-            <div className=''>
-              <h3 className='text-gray-500 text-md uppercase'>Total Requests</h3>
-              <p className='text-2xl'>{totalRequests}</p>
+        <div className="flex-wrap flex gap-4 justify-center p-3">
+          <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
+            <div className="flex justify-between">
+              <div className="">
+                <h3 className="text-gray-500 text-md uppercase">Accepted</h3>
+                <p className="text-2xl">{totalAcceptRequests}</p>
+              </div>
+              <HiGift className="bg-green-600 text-white rounded-full text-5xl p-3 shadow-lg" />
             </div>
-            <HiGift className='bg-red-600 text-white rounded-full text-5xl p-3 shadow-lg' />
           </div>
         </div>
-      </div>
 
-      <div className='flex-wrap flex gap-4 justify-center p-3'>
-        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
-          <div className='flex justify-between'>
-            <div className=''>
-              <h3 className='text-gray-500 text-md uppercase'>Total Requests</h3>
-              <p className='text-2xl'>{totalRequests}</p>
+        <div className="flex-wrap flex gap-4 justify-center p-3">
+          <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
+            <div className="flex justify-between">
+              <div className="">
+                <h3 className="text-gray-500 text-md uppercase">Finished</h3>
+                <p className="text-2xl">{totalFinishedRequests}</p>
+              </div>
+              <HiGift className="bg-red-600 text-white rounded-full text-5xl p-3 shadow-lg" />
             </div>
-            <HiGift className='bg-red-600 text-white rounded-full text-5xl p-3 shadow-lg' />
           </div>
         </div>
-      </div>
       </div>
 
       {currentUser.isAdmin && requests.length > 0 ? (
         <>
           <Table hoverable className="shadow-md">
             <Table.Head>
-              
               <Table.HeadCell>Name</Table.HeadCell>
               <Table.HeadCell>Email</Table.HeadCell>
               <Table.HeadCell>Phone</Table.HeadCell>
@@ -119,31 +153,48 @@ const formatDate = (dateString) => {
               <Table.HeadCell>Action</Table.HeadCell>
             </Table.Head>
             {requests.map((item) => (
-              <Table.Body className='divide-y' key={item._id}>
-                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
-                  
+              <Table.Body className="divide-y" key={item._id}>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                   <Table.Cell>{item.name}</Table.Cell>
                   <Table.Cell>{item.email}</Table.Cell>
                   <Table.Cell>{item.phone}</Table.Cell>
-                  <Table.Cell>{item.address}, {item.city} <br /> {item.state} <br /> {item.zip}</Table.Cell>
+                  <Table.Cell>
+                    {item.address}, {item.city} <br /> {item.state} <br />{" "}
+                    {item.zip}
+                  </Table.Cell>
                   <Table.Cell>{item.serviceType}</Table.Cell>
                   <Table.Cell>{formatDate(item.date)}</Table.Cell>
                   <Table.Cell>{item.paymentMethod}</Table.Cell>
                   <Table.Cell>
                     <div className="flex flex-col justify-center">
-                            <p>Accept</p> <Checkbox></Checkbox>
-                            <p>Mark as Completed</p> <Checkbox></Checkbox>
-                        <Link onClick={() => {
-                            setShowModel(true);
-                            setBookIdToDelete(item._id);
-                          }}>
-                            <box-icon name='x-circle' color="red"></box-icon>
-                        </Link>
+                      <div className="flex items-center">
                         
+                        <Checkbox
+                          checked={item.accepted}
+                          onChange={() => handleAcceptChange(item._id)}
+                        />
+                        <p className="ml-3">Accept</p>
+
+                      </div>
+                      <div className="flex items-center">
+                        
+                        <Checkbox
+                          checked={item.finished}
+                          onChange={() => handleFinishChange(item._id)}
+                        />
+                        <p className="ml-3">Mark as Complete</p>
+                      </div>
+                      <Link
+                        onClick={() => {
+                          setShowModel(true);
+                          setBookIdToDelete(item._id);
+                        }}
+                      >
+                        <box-icon name="x-circle" color="red"></box-icon>
+                      </Link>
+                      
                     </div>
                   </Table.Cell>
-
-                  
                 </Table.Row>
               </Table.Body>
             ))}
@@ -152,18 +203,25 @@ const formatDate = (dateString) => {
       ) : (
         <p>You have no products to show</p>
       )}
-      <Modal show={showModel} onClose={() => setShowModel(false)} popup size='md'>
+      <Modal
+        show={showModel}
+        onClose={() => setShowModel(false)}
+        popup
+        size="md"
+      >
         <Modal.Header />
         <Modal.Body>
           <div className="text-center">
             <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
-            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-200">Are you sure you want to Delete this Booking</h3>
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-200">
+              Are you sure you want to Delete this Booking
+            </h3>
           </div>
-          <div className='flex justify-center gap-4'>
-            <Button color='failure' onClick={handleDeleteBooking}>
+          <div className="flex justify-center gap-4">
+            <Button color="failure" onClick={handleDeleteBooking}>
               Yes, I am sure
             </Button>
-            <Button color='gray' onClick={() => setShowModel(false)}>
+            <Button color="gray" onClick={() => setShowModel(false)}>
               No, cancel
             </Button>
           </div>
